35cd24087bb296c23a3413764b40d8c2f7ebd3eb
Ubuntu <ubuntu@ip-172-31-57-30.ec2.internal>
2018-03-01 07:29:09 +0000

    instance listing is functional

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+'use strict'
+
+require('console.table')
+const app = require('commander')
+const owners = require('./lib/list-instance-owners.js')
+
+app
+  .version('1.0.0')
+  .option('-r, --region <region>', 'Region to list instances from')
+  .parse(process.argv)
+
+owners.list(app.region)
